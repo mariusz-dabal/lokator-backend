e9e9d27685ae25fdef27c3a87a7d9ac0f9771065
mariusz-dabal <mariusz9dabal@gmail.com>
2020-05-24 11:49:21 +0200

    Add user roles

--- app/User.php
+++ app/User.php
@@ -38,6 +38,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role');
+    }
+
     public function flat()
     {
         return $this->belongsTo('App\Flat');
